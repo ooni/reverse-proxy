082c8219ad69353ecce201d0589bf815afb16085
Arturo Filastò <arturo@filasto.net>
2024-01-18 21:43:27 +0100

    fixup variables

--- tf/environments/production/tier0/variables.tf
+++ tf/environments/production/tier0/variables.tf
@@ -1,11 +1,5 @@
-variable "region" {
+variable "aws_region" {
   description = "The AWS region to create resources in"
   type        = string
   default     = "us-west-2"
-}
-
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
 }
