ed73e87c917c7ad0b88a7a939319eeaf37fe419e
Arturo Filastò <arturo@filasto.net>
2024-01-18 22:45:00 +0100

    Improvements to the ECS config

---
+++ tf/environments/production/tier1/ecs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+echo ECS_CLUSTER=ooni-production-tier1-cluster >> /etc/ecs/ecs.config
--- tf/environments/production/tier1/main.tf
+++ tf/environments/production/tier1/main.tf
@@ -2,39 +2,240 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_ecs_cluster" "cluster" {
-  name = "prod-tier1-cluster"
+resource "aws_vpc" "main" {
+ cidr_block           = "10.0.0.0/16"
+ enable_dns_hostnames = true
+ tags = {
+   name = "main"
+ }
 }
 
-resource "aws_ecs_task_definition" "dataapi" {
-  family                   = "dataapi"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
+resource "aws_subnet" "subnet" {
+ vpc_id                  = aws_vpc.main.id
+ cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 1)
+ map_public_ip_on_launch = true
+ availability_zone       = "eu-central-1a"
+}
 
-  container_definitions = jsonencode([{
+resource "aws_subnet" "subnet2" {
+ vpc_id                  = aws_vpc.main.id
+ cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 2)
+ map_public_ip_on_launch = true
+ availability_zone       = "eu-central-1b"
+}
+
+resource "aws_internet_gateway" "internet_gateway" {
+ vpc_id = aws_vpc.main.id
+ tags = {
+   Name = "internet_gateway"
+ }
+}
+
+resource "aws_route_table" "route_table" {
+ vpc_id = aws_vpc.main.id
+ route {
+   cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.internet_gateway.id
+ }
+}
+
+resource "aws_route_table_association" "subnet_route" {
+ subnet_id      = aws_subnet.subnet.id
+ route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_route_table_association" "subnet2_route" {
+ subnet_id      = aws_subnet.subnet2.id
+ route_table_id = aws_route_table.route_table.id
+}
+
+resource "aws_security_group" "security_group" {
+ name   = "ecs-security-group"
+ vpc_id = aws_vpc.main.id
+
+ ingress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = -1
+   self        = "false"
+   cidr_blocks = ["0.0.0.0/0"]
+   description = "any"
+ }
+
+ egress {
+   from_port   = 0
+   to_port     = 0
+   protocol    = "-1"
+   cidr_blocks = ["0.0.0.0/0"]
+ }
+}
+
+resource "aws_launch_template" "ecs_lt" {
+ name_prefix   = "ecs-template"
+ image_id      = "ami-025a6a5beb74db87b"
+ instance_type = "t3.micro"
+
+ key_name               = "ec2ecsglog"
+ vpc_security_group_ids = [aws_security_group.security_group.id]
+ iam_instance_profile {
+   name = "ecsInstanceRole"
+ }
+
+ block_device_mappings {
+   device_name = "/dev/xvda"
+   ebs {
+     volume_size = 30
+     volume_type = "gp2"
+   }
+ }
+
+ tag_specifications {
+   resource_type = "instance"
+   tags = {
+     Name = "ecs-instance"
+   }
+ }
+
+ user_data = filebase64("${path.module}/ecs.sh")
+}
+
+resource "aws_autoscaling_group" "ecs_asg" {
+ vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+ desired_capacity    = 2
+ max_size            = 3
+ min_size            = 1
+
+ launch_template {
+   id      = aws_launch_template.ecs_lt.id
+   version = "$Latest"
+ }
+
+ tag {
+   key                 = "AmazonECSManaged"
+   value               = true
+   propagate_at_launch = true
+ }
+}
+
+resource "aws_lb" "ecs_alb" {
+ name               = "ecs-alb"
+ internal           = false
+ load_balancer_type = "application"
+ security_groups    = [aws_security_group.security_group.id]
+ subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+
+ tags = {
+   Name = "ecs-alb"
+ }
+}
+
+resource "aws_lb_listener" "ecs_alb_listener" {
+ load_balancer_arn = aws_lb.ecs_alb.arn
+ port              = 80
+ protocol          = "HTTP"
+
+ default_action {
+   type             = "forward"
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+ }
+}
+
+resource "aws_lb_target_group" "ecs_tg" {
+ name        = "ecs-target-group"
+ port        = 80
+ protocol    = "HTTP"
+ target_type = "ip"
+ vpc_id      = aws_vpc.main.id
+
+ health_check {
+   path = "/"
+ }
+}
+
+resource "aws_ecs_cluster" "ecs_cluster" {
+ name = "ooni-production-tier1-cluster"
+}
+
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+ name = "test1"
+
+ auto_scaling_group_provider {
+   auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+
+   managed_scaling {
+     maximum_scaling_step_size = 1000
+     minimum_scaling_step_size = 1
+     status                    = "ENABLED"
+     target_capacity           = 3
+   }
+ }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "main" {
+ cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+ capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+
+ default_capacity_provider_strategy {
+   base              = 1
+   weight            = 100
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+ }
+}
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+ family             = "my-ecs-task"
+ network_mode       = "awsvpc"
+ execution_role_arn = "arn:aws:iam::532199187081:role/ecsTaskExecutionRole"
+ cpu                = 256
+ runtime_platform {
+   operating_system_family = "LINUX"
+   cpu_architecture        = "X86_64"
+ }
+ container_definitions = jsonencode([
+   {
     name  = "dataapi",
     image = "ooni/dataapi:latest",
-    portMappings = [{
-      containerPort = 80,
-      hostPort      = 80
-    }]
-  }])
+     cpu       = 256
+     memory    = 512
+     essential = true
+     portMappings = [
+       {
+         containerPort = 80
+         hostPort      = 80
+         protocol      = "tcp"
+       }
+     ]
+   }
+ ])
 }
 
-resource "aws_iam_role" "ecs_execution_role" {
-  name = "prod_ecs_execution_role"
+resource "aws_ecs_service" "ecs_service" {
+ name            = "ooni-prod-ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+ desired_count   = 2
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      },
-    }]
-  })
+ network_configuration {
+   subnets         = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+   security_groups = [aws_security_group.security_group.id]
+ }
+
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
+
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+   container_name   = "dockergs"
+   container_port   = 80
+ }
+
+ depends_on = [aws_autoscaling_group.ecs_asg]
 }
