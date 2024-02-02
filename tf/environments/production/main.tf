8b50ecea6a6f73773068eadf6def7416f83a9a73
Arturo Filastò <arturo@filasto.net>
2024-02-02 22:05:31 +0100

    Fix missing variable
    Use terraform_state_backend module to create backend
    
    Add docs on how to use this
    
    Comment out event_name filter
    
    Fix commenting action
    
    hotifx
    
    Update to new github output pattern
    
    Run terraform fmt
    
    Update the last comment instead of creating a new one
    
    Fix GITHUB multiline output
    
    Also, tables are nice
    
    Fix editing of comment?
    
    Fix bugs

--- tf/environments/production/main.tf
+++ tf/environments/production/main.tf
@@ -1,18 +1,35 @@
 # Store terraform state in s3
 terraform {
   backend "s3" {
-    bucket = "ooni-secrets"
-    key    = "terraform/production/terraform-production.tfstate"
-    region = "eu-central-1"
+    region  = "eu-central-1"
+    bucket  = "ooni-production-terraform-state"
+    key     = "terraform.tfstate"
+    profile = ""
+    encrypt = "true"
+
+    dynamodb_table = "ooni-production-terraform-state-lock"
   }
+}
 
-  #cloud {
-  #    organization = "ooni"
-  #    workspaces {
-  #        name = "production-tier1"
-  #    }
-  #}
+# You cannot create a new backend by simply defining this and then
+# immediately proceeding to "terraform apply". The S3 backend must
+# be bootstrapped according to the simple yet essential procedure in
+# https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
+# You cannot create a new backend by simply defining this and then
+# immediately proceeding to "terraform apply". The S3 backend must
+# be bootstrapped according to the simple yet essential procedure in
+# https://github.com/cloudposse/terraform-aws-tfstate-backend#usage
+module "terraform_state_backend" {
+  source     = "cloudposse/tfstate-backend/aws"
+  version    = "1.4.0"
+  namespace  = "ooni"
+  stage      = "production"
+  name       = "terraform"
+  attributes = ["state"]
 
+  #terraform_backend_config_file_path = "."
+  terraform_backend_config_file_name = "backend.tf"
+  force_destroy                      = false
 }
 
 provider "aws" {
