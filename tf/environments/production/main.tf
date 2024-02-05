74387c98b3f8912edd3c9bea43f0cfea98f7635a
Arturo Filastò <arturo@filasto.net>
2024-02-05 09:37:45 +0100

    Run terraform fmt

--- tf/environments/production/main.tf
+++ tf/environments/production/main.tf
@@ -39,7 +39,7 @@ resource "local_file" "ansible_inventory" {
     aws_route53_record.clickhouse_dns
   ]
 
-  content  = templatefile("${path.module}/templates/ansible-inventory.tpl", {
+  content = templatefile("${path.module}/templates/ansible-inventory.tpl", {
     clickhouse_servers = [
       local.clickhouse_hostname
     ]
@@ -48,12 +48,12 @@ resource "local_file" "ansible_inventory" {
 }
 
 resource "null_resource" "ansible_update_known_hosts" {
-  depends_on = [ local_file.ansible_inventory ]
+  depends_on = [local_file.ansible_inventory]
 
   provisioner "local-exec" {
     command = "./scripts/update_known_hosts.sh"
     environment = {
-      INVENTORY_FILE = "ansible/inventory.ini"
+      INVENTORY_FILE   = "ansible/inventory.ini"
       KNOWN_HOSTS_FILE = "ansible/known_hosts"
     }
   }
