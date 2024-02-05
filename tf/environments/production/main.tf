a6d28803681f475e9b2a9cf6dc97e63742f9cfd5
Arturo Filastò <arturo@filasto.net>
2024-02-05 12:10:51 +0100

    Allow 443 traffic on load balancer

--- tf/environments/production/main.tf
+++ tf/environments/production/main.tf
@@ -308,6 +308,13 @@ resource "aws_security_group" "lb_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 443
+    to_port     = 443
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 0
