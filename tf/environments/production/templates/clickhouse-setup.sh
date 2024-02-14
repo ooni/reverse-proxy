d4e22fc99e26be5241f0a7b70555fc442a7c8671
Arturo Filastò <arturo@filasto.net>
2024-02-14 17:22:27 +0100

    Add support for persistent data volume (#2)
    * add basic readme file
    * also fix deploy checks when a branch lands on master, since it fails
    because the comment script doesn't have in it the reference to the open
    PR number

--- tf/environments/production/templates/clickhouse-setup.sh
+++ tf/environments/production/templates/clickhouse-setup.sh
@@ -4,9 +4,9 @@ sudo hostnamectl set-hostname --static ${hostname}
 # Install datadog agent
 DD_API_KEY=${datadog_api_key} DD_SITE="datadoghq.eu" bash -c "$(curl -L https://s3.amazonaws.com/dd-agent/scripts/install_script_agent7.sh)"
 
-sudo mkfs.ext4 -q -F ${device_name}
+# This only needs to be run the first time to initialize the volume
+# sudo mkfs.ext4 -q -F ${device_name}
 sudo mkdir -p /var/lib/clickhouse
 sudo mount ${device_name} /var/lib/clickhouse
 echo "${device_name} /var/lib/clickhouse ext4 defaults,nofail 0 2" | sudo tee -a /etc/fstab
 sudo chown -R clickhouse:clickhouse /var/lib/clickhouse
-
