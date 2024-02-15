313024e3031c73cffb3865c21e404f72bff44d88
Arturo Filastò <arturo@filasto.net>
2024-02-15 10:00:49 +0100

    Drop db name and use default

--- tf/environments/production/main.tf
+++ tf/environments/production/main.tf
@@ -285,7 +285,6 @@ resource "aws_db_instance" "ooni_pg" {
   engine_version          = "16.1"
   instance_class          = "db.t3.micro"
   identifier              = "ooni-postgresql-tier0-prod"
-  db_name                 = "oonipg"
   username                = "oonipg"
   password                = var.ooni_pg_password
   parameter_group_name    = "default.postgres16"
