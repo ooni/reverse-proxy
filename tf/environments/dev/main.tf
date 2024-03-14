2844d98d4c9dee5f0a764d1fcd9a16409bf03997
Arturo Filastò <arturo@filasto.net>
2024-03-14 22:25:04 +0100

    Change email address sender

--- tf/environments/dev/main.tf
+++ tf/environments/dev/main.tf
@@ -158,7 +158,7 @@ resource "aws_route53_record" "postgres_dns" {
 module "ooniapi_user" {
   source = "../../modules/ooniapi_user"
 
-  email_address = "admin+dev@ooni.org"
+  email_address = "admin@dev.ooni.io"
   tags          = local.tags
 }
 
