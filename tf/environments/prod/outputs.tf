8c042901fdcde9a64125be8c43a044273d38880f
Arturo Filastò <arturo@filasto.net>
2024-04-08 16:26:07 +0200

    Initialize prod environment

---
+++ tf/environments/prod/outputs.tf
@@ -0,0 +1,23 @@
+output "oonidevops_role_arn" {
+  value = module.adm_iam_roles.oonidevops_role_arn
+}
+
+output "oonidevops_key_name" {
+  value = module.adm_iam_roles.oonidevops_key_name
+}
+
+output "oonidevops_deploy_key_arn" {
+  value = module.adm_iam_roles.oonidevops_deploy_key_arn
+}
+
+output "oonipg_pg_password_arn" {
+  value = module.oonipg.secrets_manager_pg_password_id
+}
+
+# output "oonidataapi_alb_hostname" {
+#   value = module.ooni_dataapi.alb_dns_name
+# }
+
+# output "db_instance_endpoint" {
+#   value = module.postgresql.endpoint
+# }
