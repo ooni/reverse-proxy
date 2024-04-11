898ad72237ce9ba290940307060211ccef4f6ebf
Arturo Filastò <arturo@filasto.net>
2024-04-11 10:39:44 +0200

    Add mehul to authorized users

--- tf/environments/prod/main.tf
+++ tf/environments/prod/main.tf
@@ -73,7 +73,8 @@ module "adm_iam_roles" {
   source = "../../modules/adm_iam_roles"
 
   authorized_accounts = [
-    "arn:aws:iam::${local.ooni_main_org_id}:user/art"
+    "arn:aws:iam::${local.ooni_main_org_id}:user/art",
+    "arn:aws:iam::${local.ooni_main_org_id}:user/mehul"
   ]
 }
 
