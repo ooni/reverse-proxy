53ff155338824ead854f278065a3336021d3bfc0
Arturo Filastò <arturo@filasto.net>
2024-04-17 16:07:58 +0200

    Enable egress only IPv6 route

--- tf/modules/network/main.tf
+++ tf/modules/network/main.tf
@@ -23,14 +23,26 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
+resource "aws_egress_only_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = var.tags
+}
+
+
 resource "aws_route_table" "r" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block      = "0.0.0.0/0"
-    ipv6_cidr_block = "::/0"
-    gateway_id      = aws_internet_gateway.gw.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
   }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    egress_only_gateway_id = aws_egress_only_internet_gateway.gw.id
+  }
+
 
   tags = var.tags
 }
