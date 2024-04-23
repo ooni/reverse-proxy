0d1bd4e2f56f4eabe594474470ddd6b49e39683d
Arturo Filastò <arturo@filasto.net>
2024-04-23 08:27:02 +0200

    Create route table before destroy

--- tf/modules/network/main.tf
+++ tf/modules/network/main.tf
@@ -139,4 +139,8 @@ resource "aws_route_table_association" "private" {
   count          = var.az_count
   subnet_id      = element(aws_subnet.private[*].id, count.index)
   route_table_id = element(aws_route_table.private[*].id, count.index)
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
