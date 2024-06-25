1015cfacb2986122099263e7f1d19abb4d2a10ea
Arturo Filastò <arturo@filasto.net>
2024-06-25 11:30:17 +0200

    Modify ooni.io MX record

--- tf/environments/prod/dns_records.tf
+++ tf/environments/prod/dns_records.tf
@@ -688,8 +688,8 @@ resource "aws_route53_record" "ooni-io-_A_" {
 
 resource "aws_route53_record" "ooni-io-_MX_" {
   name    = "ooni.io"
-  records = ["1 ASPMX.L.GOOGLE.COM.ooni.io", "10 ALT3.ASPMX.L.GOOGLE.COM.ooni.io", "10 ALT4.ASPMX.L.GOOGLE.COM.ooni.io", "5 ALT1.ASPMX.L.GOOGLE.COM.ooni.io", "5 ALT2.ASPMX.L.GOOGLE.COM.ooni.io"]
-  ttl     = "1799"
+  records = ["1 smtp.google.com"]
+  ttl     = "300"
   type    = "MX"
   zone_id = local.dns_root_zone_ooni_io
 }
