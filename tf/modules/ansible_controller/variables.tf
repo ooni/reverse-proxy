498d7bba4241a29d0413181758586dd49c121aa9
Arturo Filastò <arturo@filasto.net>
2024-07-02 15:22:57 +0200

    Remove name variable

--- tf/modules/ansible_controller/variables.tf
+++ tf/modules/ansible_controller/variables.tf
@@ -12,11 +12,6 @@ variable "key_name" {
   description = "Name of AWS key pair"
 }
 
-variable "name" {
-  description = "Name of the resources"
-  default     = "ooni-backendproxy"
-}
-
 variable "instance_type" {
   default = "t2.micro"
 }
