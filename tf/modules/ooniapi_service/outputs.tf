f51888f30fca46a2e7e1bba5b591b4021e09eeb0
decfox <mehul@openobservatory.org>
2024-09-06 02:44:35 +0530

    refactor: remove individual load balancers from services

--- tf/modules/ooniapi_service/outputs.tf
+++ tf/modules/ooniapi_service/outputs.tf
@@ -1,11 +1,3 @@
-output "ooni_io_fqdn" {
-  value = aws_route53_record.ooniapi_service.name
-}
-
-output "dns_name" {
-  value = aws_alb.ooniapi_service.dns_name
-}
-
 output "ecs_service_name" {
   value = aws_ecs_service.ooniapi_service.name
 }
