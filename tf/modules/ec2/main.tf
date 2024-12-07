a6d684317184ae9d8e0268f85dda6f148bec6fa5
decfox <mehul@openobservatory.org>
2024-12-07 18:47:44 -0500

    feat: init common module for ec2 machines

--- tf/modules/ec2/main.tf
+++ tf/modules/ec2/main.tf
@@ -4,9 +4,9 @@ data "aws_ssm_parameter" "ubuntu_22_ami" {
 
 # Important note about security groups:
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/security_group#recreating-a-security-group
-resource "aws_security_group" "nginx_sg" {
-  description = "security group for nginx"
-  name_prefix = "ooni-bckprx"
+resource "aws_security_group" "ec2_sg" {
+  description = "security group for ec2"
+  name_prefix = var.sg_prefix
 
   vpc_id = var.vpc_id
 
@@ -54,30 +54,24 @@ resource "aws_security_group" "nginx_sg" {
   tags = var.tags
 }
 
-data "cloudinit_config" "ooni_backendproxy" {
+data "cloudinit_config" "ooni_ec2" {
   base64_encode = true
 
   part {
     filename     = "init.cfg"
     content_type = "text/cloud-config"
-    content = templatefile("${path.module}/templates/cloud-init.yml", {
-      wcth_addresses     = var.wcth_addresses,
-      wcth_domain_suffix = var.wcth_domain_suffix,
-      backend_url        = var.backend_url,
-      clickhouse_url     = var.clickhouse_url,
-      clickhouse_port    = var.clickhouse_port
-    })
+    content = templatefile("${path.module}/templates/cloud-init.yml", {})
   }
 
 }
 
-resource "aws_launch_template" "ooni_backendproxy" {
-  name_prefix   = "${var.name}-bkprx-tmpl-"
+resource "aws_launch_template" "ooni_ec2" {
+  name_prefix   = "${var.name}-tmpl-"
   image_id      = data.aws_ssm_parameter.ubuntu_22_ami.value
   instance_type = var.instance_type
   key_name      = var.key_name
 
-  user_data = data.cloudinit_config.ooni_backendproxy.rendered
+  user_data = data.cloudinit_config.ooni_ec2.rendered
 
   lifecycle {
     create_before_destroy = true
@@ -88,7 +82,7 @@ resource "aws_launch_template" "ooni_backendproxy" {
     associate_public_ip_address = true
     subnet_id                   = var.subnet_id
     security_groups = [
-      aws_security_group.nginx_sg.id,
+      aws_security_group.ec2_sg.id,
     ]
   }
 
@@ -98,9 +92,9 @@ resource "aws_launch_template" "ooni_backendproxy" {
   }
 }
 
-resource "aws_instance" "oonibackend_proxy" {
+resource "aws_instance" "ooni_ec2" {
   launch_template {
-    id      = aws_launch_template.ooni_backendproxy.id
+    id      = aws_launch_template.ooni_ec2.id
     version = "$Latest"
   }
 
@@ -111,8 +105,8 @@ resource "aws_instance" "oonibackend_proxy" {
   tags = var.tags
 }
 
-resource "aws_alb_target_group" "oonibackend_proxy" {
-  name_prefix = "oobpx"
+resource "aws_alb_target_group" "ooni_ec2" {
+  name_prefix = "oo${var.tg_prefix}"
   port        = 80
   protocol    = "HTTP"
   vpc_id      = var.vpc_id
@@ -125,17 +119,6 @@ resource "aws_alb_target_group" "oonibackend_proxy" {
 }
 
 resource "aws_lb_target_group_attachment" "oonibackend_proxy" {
-  target_id        = aws_instance.oonibackend_proxy.id
-  target_group_arn = aws_alb_target_group.oonibackend_proxy.arn
-}
-
-resource "aws_route53_record" "clickhouse_proxy_alias" {
-  zone_id = var.dns_zone_ooni_io
-  name    = "clickhouseproxy.${var.stage}.ooni.io"
-  type    = "CNAME"
-  ttl     = 300
-
-  records = [
-    aws_instance.oonibackend_proxy.public_dns
-  ]
+  target_id        = aws_instance.ooni_ec2
+  target_group_arn = aws_alb_target_group.ooni_ec2.arn
 }
