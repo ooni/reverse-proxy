adf63e57c82a148377505f9573523f4a5bb75609
decfox <mehul@openobservatory.org>
2024-12-10 09:19:10 -0500

    fix: cidr blocks arrays for ec2 module

--- tf/modules/ec2/main.tf
+++ tf/modules/ec2/main.tf
@@ -24,8 +24,8 @@ resource "aws_security_group_rule" "ec2_sg_ingress" {
   from_port         = var.ingress_rules[count.index].from_port
   to_port           = var.ingress_rules[count.index].to_port
   protocol          = var.ingress_rules[count.index].protocol
-  cidr_blocks       = [var.ingress_rules[count.index].cidr_blocks]
-  ipv6_cidr_blocks  = [var.ingress_rules[count.index].ipv6_cidr_blocks]
+  cidr_blocks       = var.ingress_rules[count.index].cidr_blocks
+  ipv6_cidr_blocks  = var.ingress_rules[count.index].ipv6_cidr_blocks
   security_group_id =  aws_security_group.ec2_sg.id
 }
 
